9638bc40b63026cad80fc1da409de8579f7cc8a0
Timothy Perera <timothyperera.m@gmail.com>
2021-02-17 18:17:19 +0400

    add register and form files

--- flaskblog.py
+++ flaskblog.py
@@ -7,7 +7,11 @@ Created on Sat Feb 13 14:57:27 2021
 """
 
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c482cc4f46913c25b396ef97dc51cb7b'
 
 posts = [
     {
@@ -27,13 +31,23 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',posts = posts)
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+    
 if __name__ == '__main__':
 	app.run(debug=True)
 
