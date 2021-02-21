b86f6f792ab6930c54056c7f2966733f352994b3
TimPerera <timothyperera.m@gmail.com>
2021-02-21 22:23:49 +0400

    add sql database

--- flaskblog.py
+++ flaskblog.py
@@ -5,13 +5,38 @@ Created on Sat Feb 13 14:57:27 2021
 
 @author: timothy
 """
-
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'c482cc4f46913c25b396ef97dc51cb7b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary key ensures that data in this column is unique
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # foreign key provides a link between data in two tables
+
+    def __repr__(self):
+        return f"User('{self.title}','{self.date_posted}')"
+
+  
 
 posts = [
     {
